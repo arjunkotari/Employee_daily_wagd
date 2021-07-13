0824efc06711154d85b7b8575a0f4ce62c584d5f
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 14:52:14 +0530

    Added: Calculating Employee Wage Using Switch Case

--- src/EmployeeDailyWage/Employee_Daily_wage.java
+++ src/EmployeeDailyWage/Employee_Daily_wage.java
@@ -1,23 +1,32 @@
 package EmployeeDailyWage;
 
 public class Employee_Daily_wage {
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
+	
 	public static void main(String[] args) {
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
-
-		// Variables
-		int empHrs=0;
-		int empWage=0;
-		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs=4;
-		else
-			empHrs=0;
-		empWage	= empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " +empWage);
+		int empHrs=0, empWage=0, totalEmpWage=0;
+		
+		for(int day=0; day<NUM_OF_WORKING_DAYS;day++)
+		{
+			int empCheck = (int)Math.floor(Math.random() *10)%3;
+			switch(empCheck)
+			{
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage+= empWage;
+			System.out.println("Emp Wage : "+empWage);
+		}
+		System.out.println("Total Emp Wage: "+totalEmpWage);
 	}
 }
