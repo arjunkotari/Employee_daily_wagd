461dc76cdb4968b5ae862c1c9e38f2470fff16fb
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 14:10:51 +0530

    Added:Employee Attendence

--- src/EmployeeDailyWage/Employee_Daily_wage.java
+++ src/EmployeeDailyWage/Employee_Daily_wage.java
@@ -3,5 +3,11 @@ package EmployeeDailyWage;
 public class Employee_Daily_wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Daily Wage");
+		int is_full_time=1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == is_full_time)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
